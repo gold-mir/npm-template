b69482bf6e0b284f13fae39677dc294eebe4f972
Miranda <miranda.keith.42@gmail.com>
2019-01-09 14:44:28 -0800

    add correct version of babel

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,14 @@ module.exports = {
         test: /\.js$/,
         exclude: [/node_modules/, /spec/],
         loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /spec/],
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
       }
     ]
   }
